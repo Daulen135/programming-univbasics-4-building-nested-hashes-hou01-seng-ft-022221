8280093796546fb09ee0398b7edf91f7ee3750c9
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-05 16:18:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,32 +48,33 @@ def monopoly_with_third_tier
 
 def monopoly_with_fourth_tier
   monopoly={
-  :railroads=>{
+    :railroads=>{
   
-  :pieces => 4,
-  :rent_in_dollars => {
-    :one_piece_owned => 25,
-    :two_pieces_owned => 50,
-    :three_pieces_owned => 100,
-    :four_pieces_owned => 200
+      :pieces => 4,
+      
+      :rent_in_dollars => {
+          :one_piece_owned => 25,
+          :two_pieces_owned => 50,
+          :three_pieces_owned => 100,
+          :four_pieces_owned => 200
     
-  }, #close rent_in_dollars
-  :names => {
-    :reading_railroad =>{
-      :mortgage_value =>100,
-    },
-    :pennsylvania_railroad =>{
-      :mortgage_value => 200,
-    },
-    :b_and_o_railroad =>{
-      :mortgage_value => 400,
-    },
-    :shortline_railroad =>{
-      :mortgage_value => 800
-    }
-  } #close names
+      }, #close rent_in_dollars
+      :names => {
+          :reading_railroad =>{
+            :mortgage_value =>100,
+          },
+          :pennsylvania_railroad =>{
+            :mortgage_value => 200,
+          },
+          :b_and_o_railroad =>{
+            :mortgage_value => 400,
+          },
+          :shortline_railroad =>{
+            :mortgage_value => 800
+           }
+      } #close names
 
-  } #close rairoads
+    } #close rairoads
   } #close monopoly 
 
 end
